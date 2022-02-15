9978988b14fa1a0be089953de68ca3a7b3cbb80e
Tom Harris <tomharris3@me.com>
2022-02-15 15:30:52 +0000

    restructured files and folders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,19 +11,19 @@ afterAll(() => {
 
 // 500 server error
 
-
-describe('/api', () => {
-    describe('Universal errors', () => {
-        test('status: 404, path does not exist', () => {
-            return request(app)
-            .get('/api/notARoute')
-            .expect(404)
-            .then(({body}) => {
-                expect(body.msg).toBe('Invalid path')
-            })
+describe('Universal errors', () => {
+    test('status: 404, path does not exist', () => {
+        return request(app)
+        .get('/api/notARoute')
+        .expect(404)
+        .then(({body}) => {
+            expect(body.msg).toBe('Invalid path')
         })
-    });
-    describe('GET /topics', () => {
+    })
+});
+describe('GET Requests', () => {
+    
+    describe('/topics', () => {
         test('Status: 200, responds with an array of topics', () => {
             return request(app)
             .get('/api/topics')
@@ -48,44 +48,74 @@ describe('/api', () => {
             })
         })
     });
-    describe('GET /articles', () => {
-        describe('/api/articles/:article_id', () => {
-            test('status: 200, responds with article object', () => {
-                const article_id = 1;
-                return request(app)
-                .get(`/api/articles/${article_id}`)
-                .expect(200)
-                .then(({body}) => {
-                    expect(body.article).toEqual( {
-                        author: "butter_bridge",
-                        title: "Living in the shadow of a great man",
-                        article_id: 1,
-                        body: "I find this existence challenging",
-                        topic: "mitch",
-                        created_at: "2020-07-09T20:11:00.000Z",
-                        votes: 100,
-                    })
+    describe('/articles', () => {
+        test('status: 200, responds with article object', () => {
+            const article_id = 1;
+            return request(app)
+            .get(`/api/articles/${article_id}`)
+            .expect(200)
+            .then(({body}) => {
+                expect(body.article).toEqual( {
+                    author: "butter_bridge",
+                    title: "Living in the shadow of a great man",
+                    article_id: 1,
+                    body: "I find this existence challenging",
+                    topic: "mitch",
+                    created_at: "2020-07-09T20:11:00.000Z",
+                    votes: 100,
                 })
             })
-            test('status: 404, item not found', () => {
-                const articleId = 9999999
-                return request(app)
-                .get(`/api/articles/${articleId}`)
-                .expect(404)
-                .then(({body}) => {
-                    expect(body.msg).toBe('Item not found')
-                })
+        })
+        test('status: 404, item not found', () => {
+            const articleId = 9999999
+            return request(app)
+            .get(`/api/articles/${articleId}`)
+            .expect(404)
+            .then(({body}) => {
+                expect(body.msg).toBe('Item not found')
             })
-            test('status: 400, invalid ID', () => {
-                const articleId = 'banana'
-                return request(app)
-                .get(`/api/articles/${articleId}`)
-                .expect(400)
-                .then(({body: {msg}}) => {
-                    // console.log(msg)
-                    expect(msg).toBe('Invalid ID')
-                })
+        })
+        test('status: 400, invalid ID', () => {
+            const articleId = 'banana'
+            return request(app)
+            .get(`/api/articles/${articleId}`)
+            .expect(400)
+            .then(({body: {msg}}) => {
+                // console.log(msg)
+                expect(msg).toBe('Invalid ID')
             })
-        });
+        })
+    });
+});
+
+describe('Increment article vote count by artice id', () => {
+    test.only('responds with updated article increased by one', () => {
+        const articleId = 2
+        const newVote = { inc_votes: 1 }
+
+        return request(app)
+        .patch(`/api/articles/${articleId}`) // callback?
+        .send(newVote)
+        .expect(202)
+        .then(({body: {updatedArticle}}) => {
+            expect(updatedArticle).toEqual({
+                article_id: 2,
+                author: "icellusedkars",
+                title: "Sony Vaio; or, The Laptop",
+                body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+                topic: "mitch",
+                created_at: '2020-10-16T05:03:00.000Z',
+                votes: 1,
+              })
+            })
+        })
+    })
+    test('article vote increments by more than one', () => {
+        
+    test('article votes can decrement by any given amount', () => {
+        
+    });
+    test('should note mutate data??!', () => {
+        
     });
 });
---
+++ controllers/articles-controller.js
@@ -0,0 +1,23 @@
+const {selectArticle, incrementVote } = require('../models/articles-model')
+
+exports.getArticle = (req, res, next) => {
+    const {article_id: articleId} = req.params
+
+    selectArticle(articleId)
+    .then((article) => {
+        res.status(200).send({article})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+
+exports.updateArticleVoteCount = (req, res) => {
+    const {article_id: articleId } = req.params;
+    const {inc_votes: newVote} = req.body
+
+    incrementVote(articleId, newVote)
+    .then((updatedArticle) => {
+        res.status(202).send({updatedArticle})
+    })
+}
