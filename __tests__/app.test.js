03855f35ded847c47262b7412d543f7b01c93f77
Tom Harris <tomharris3@me.com>
2022-02-16 10:49:27 +0000

    update test file structure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,80 +12,87 @@ afterAll(() => {
 // 500 server error
 
 
-describe('/api', () => {
-    describe('Universal errors', () => {
-        test('status: 404, path does not exist', () => {
+
+describe('Universal errors', () => {
+    test('status: 404, path does not exist', () => {
+        return request(app)
+        .get('/api/notARoute')
+        .expect(404)
+        .then(({body}) => {
+            expect(body.msg).toBe('Invalid path')
+        })
+    })
+});
+describe('GET /topics', () => {
+    test('Status: 200, responds with an array of topics', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({body}) => {
+            expect(body.topics).toHaveLength(3)
+        })
+    })        
+    test('Status: 200, each item looks correct', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({body: {topics}}) => {
+            topics.forEach(topic => {
+                expect(topic).toEqual(
+                    expect.objectContaining({
+                        description: expect.any(String),
+                        slug: expect.any(String)
+                    })
+                )
+            })
+        })
+    })
+});
+describe('GET /articles', () => {
+    describe('/api/articles/:article_id', () => {
+        test('status: 200, responds with article object', () => {
+            const article_id = 1;
             return request(app)
-            .get('/api/notARoute')
+            .get(`/api/articles/${article_id}`)
+            .expect(200)
+            .then(({body}) => {
+                expect(body.article).toEqual( {
+                    author: "butter_bridge",
+                    title: "Living in the shadow of a great man",
+                    article_id: 1,
+                    body: "I find this existence challenging",
+                    topic: "mitch",
+                    created_at: "2020-07-09T20:11:00.000Z",
+                    votes: 100,
+                })
+            })
+        })
+        test('status: 404, item not found', () => {
+            const articleId = 9999999
+            return request(app)
+            .get(`/api/articles/${articleId}`)
             .expect(404)
             .then(({body}) => {
-                expect(body.msg).toBe('Invalid path')
+                expect(body.msg).toBe('Item not found')
+            })
+        })
+        test('status: 400, invalid ID', () => {
+            const articleId = 'banana'
+            return request(app)
+            .get(`/api/articles/${articleId}`)
+            .expect(400)
+            .then(({body: {msg}}) => {
+                // console.log(msg)
+                expect(msg).toBe('Invalid ID')
             })
         })
     });
-    describe('GET /topics', () => {
-        test('Status: 200, responds with an array of topics', () => {
-            return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(({body}) => {
-                expect(body.topics).toHaveLength(3)
-            })
-        })        
-        test('Status: 200, each item looks correct', () => {
-            return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(({body: {topics}}) => {
-                topics.forEach(topic => {
-                    expect(topic).toEqual(
-                        expect.objectContaining({
-                            description: expect.any(String),
-                            slug: expect.any(String)
-                        })
-                    )
-                })
-            })
-        })
-    });
-    describe('GET /articles', () => {
-        describe('/api/articles/:article_id', () => {
-            test('status: 200, responds with article object', () => {
-                const article_id = 1;
-                return request(app)
-                .get(`/api/articles/${article_id}`)
-                .expect(200)
-                .then(({body}) => {
-                    expect(body.article).toEqual( {
-                        author: "butter_bridge",
-                        title: "Living in the shadow of a great man",
-                        article_id: 1,
-                        body: "I find this existence challenging",
-                        topic: "mitch",
-                        created_at: "2020-07-09T20:11:00.000Z",
-                        votes: 100,
-                    })
-                })
-            })
-            test('status: 404, item not found', () => {
-                const articleId = 9999999
-                return request(app)
-                .get(`/api/articles/${articleId}`)
-                .expect(404)
-                .then(({body}) => {
-                    expect(body.msg).toBe('Item not found')
-                })
-            })
-            test('status: 400, invalid ID', () => {
-                const articleId = 'banana'
-                return request(app)
-                .get(`/api/articles/${articleId}`)
-                .expect(400)
-                .then(({body: {msg}}) => {
-                    // console.log(msg)
-                    expect(msg).toBe('Invalid ID')
-                })
-            })
-        });
-    });
+});
+
+
+
+describe('GET /users', () => {
+  test('', () => {
+    
+  })
 });
