e00f96610a234ccb3968bf175a2d5073953ba608
Tom Harris <tomharris3@me.com>
2022-02-17 11:22:29 +0000

    added statement terminator

--- models/articles-model.js
+++ models/articles-model.js
@@ -2,7 +2,7 @@ const db = require('../db/connection')
 
 exports.selectArticles = async () => {
   const response = await db.query(
-    "SELECT * FROM articles ORDER BY articles.created_at DESC"
+    "SELECT * FROM articles ORDER BY articles.created_at DESC;"
   );
   return response.rows;
 };
