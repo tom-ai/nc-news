d359562e4210a1d601474289c4cacbd70a76aa3e
Tom Harris <tomharris3@me.com>
2022-03-05 10:12:24 +0100

    new heroku app

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
-const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+
+const { Pool } = require('pg');
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
