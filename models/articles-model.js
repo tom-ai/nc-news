6020be3c25fe923ad340dc90fcc088877ffe1f79
Tom Harris <tomharris3@me.com>
2022-03-11 09:32:10 +0000

    fixed update article vote count

--- models/articles-model.js
+++ models/articles-model.js
@@ -1,20 +1,18 @@
 const db = require("../db/connection");
-const format = require('pg-format')
-
+const format = require("pg-format");
 
 exports.selectArticleById = async (articleId) => {
-  
-  const {rows: articles} = await db
-  .query(`
+  const { rows: articles } = await db.query(
+    `
   SELECT articles.*,
   COUNT(comments.comment_id) AS comment_count
   FROM articles
   LEFT JOIN comments ON comments.article_id = articles.article_id
   WHERE articles.article_id = $1
   GROUP BY articles.article_id;
-  `, [
-      articleId,
-    ])
+  `,
+    [articleId]
+  );
 
   if (articles.length === 0) {
     return Promise.reject({ status: 404, msg: "Not found" });
@@ -31,52 +29,58 @@ exports.incrementVote = async (articleId, vote) => {
       articleId,
     ])
     .then(({ rows: [article] }) => {
-      article.votes = vote;
-      return article; 
+      article.votes += vote;
+      return article;
     });
 };
 
 exports.selectArticles = async (
-  
-  sort_by = 'created_at',
-  order = 'desc',
+  sort_by = "created_at",
+  order = "desc",
   filter
-  ) => {
-    if (!['article_id', 'author', 'title', 'body', 'topic', 'created_at', 'votes'].includes(sort_by)) {
-      return Promise.reject({status: 400, msg: "Invalid sort_by query"})
-    }
+) => {
+  if (
+    ![
+      "article_id",
+      "author",
+      "title",
+      "body",
+      "topic",
+      "created_at",
+      "votes",
+    ].includes(sort_by)
+  ) {
+    return Promise.reject({ status: 400, msg: "Invalid sort_by query" });
+  }
 
-    if (!['asc', 'desc'].includes(order)) {
-      return Promise.reject({status: 400, msg: "Invalid order query"})
-    }
-    
+  if (!["asc", "desc"].includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order query" });
+  }
 
-    let queryStr = format(`SELECT articles.*,
+  let queryStr = format(`SELECT articles.*,
     COUNT(comments.comment_id) AS comment_count
     FROM articles
-    LEFT JOIN comments ON comments.article_id = articles.article_id`)
+    LEFT JOIN comments ON comments.article_id = articles.article_id`);
 
-    if (filter) {
-      const topics = await db.query('SELECT slug FROM topics;')
-      const topicSlugs = topics.rows.map((topic) => {
-        const { slug } = topic;
-        return slug
-      })
-  
-      if (!topicSlugs.includes(filter)) {
-        return Promise.reject({status: 400, msg: 'Invalid filter query'})
-      }
-      queryStr += format(`
-      WHERE articles.topic = '${filter}'`)
+  if (filter) {
+    const topics = await db.query("SELECT slug FROM topics;");
+    const topicSlugs = topics.rows.map((topic) => {
+      const { slug } = topic;
+      return slug;
+    });
+
+    if (!topicSlugs.includes(filter)) {
+      return Promise.reject({ status: 400, msg: "Invalid filter query" });
     }
-    
     queryStr += format(`
+      WHERE articles.topic = '${filter}'`);
+  }
+
+  queryStr += format(`
     GROUP BY articles.article_id
     ORDER BY articles.${sort_by} ${order};
-    `)
+    `);
 
-  const {rows: articles} = await db.query(queryStr);
+  const { rows: articles } = await db.query(queryStr);
   return articles;
 };
-
-
